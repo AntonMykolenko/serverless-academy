9f54f8763fd607aaead03e102f853f18e29934a1
Anton Mykolenko <119318292+gaqpy@users.noreply.github.com>
2023-10-26 12:18:03 +0300

    minor fixes

--- 03_cli_telegram_sender/app.js
+++ 03_cli_telegram_sender/app.js
@@ -14,14 +14,31 @@ program
     .description("send message to bot")
     .argument("<string>", "message to send")
     .action((message) => {
-        bot.sendMessage(chatID, message);
+        bot.sendMessage(chatID, message)
+            .then(() => {
+                console.log("Message sent successfully.");
+                process.exit(0); 
+            })
+            .catch((error) => {
+                console.error("Error sending the message:", error);
+                process.exit(1); 
+            });
     });
+
 program
     .command("send-photo")
     .description("send image to bot")
     .argument("<string>", "path to photo")
     .action((pathToPhoto) => {
-        bot.sendPhoto(chatID, pathToPhoto);
+        bot.sendPhoto(chatID, pathToPhoto)
+            .then(() => {
+                console.log("Photo sent successfully.");
+                process.exit(0);
+            })
+            .catch((error) => {
+                console.error("Error sending the photo:", error);
+                process.exit(1); 
+            });
     });
 
 program.parse();
