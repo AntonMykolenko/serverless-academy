24c9250aaa86ac940a2ad72c6a2eb024d0a6b7eb
Anton Mykolenko <119318292+gaqpy@users.noreply.github.com>
2023-11-05 17:09:43 +0200

    fix: reworked withour dotenv

--- 10_ip_geoposition/server.ts
+++ 10_ip_geoposition/server.ts
@@ -1,7 +1,4 @@
 import app from "./app";
-import { config } from 'dotenv';
 
-config();
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`App is listening on port ${port}`));
